0a695796db63be6841a885a7db6536e90a07a097
lycpaul <ycleepaul@gmail.com>
2017-05-28 20:32:52 +0800

    v2 added ps4 control

--- app/src/main/java/com/example/yuchung/robocon_apps/MainActivity.java
+++ app/src/main/java/com/example/yuchung/robocon_apps/MainActivity.java
@@ -23,6 +23,7 @@ import java.net.URISyntaxException;
  * @datetime 2017-04-06
  */
 
+
 public class MainActivity extends AppCompatActivity {
 
     private String[] value_L_list = {"value1", "value2", "value3", "value4", "value5", "value6", "value7", "value8"};
@@ -44,38 +45,14 @@ public class MainActivity extends AppCompatActivity {
     private String cat_string_temp;
     private String write_buff = "";
 
+    private PS4 ps4 = new PS4();
+
     private String cat_string(String[] a) {
         cat_string_temp = "";
         for (int i = 0; i < a.length; i++)
             cat_string_temp = cat_string_temp + a[i] + "\n";
         return cat_string_temp;
     }
-
-    /*typedef struct __attribute__ ((packed)) {
-            uint8_t hat_left_x;
-            uint8_t hat_left_y;
-            uint8_t hat_right_x;
-            uint8_t hat_right_y;
-
-            uint8_t dpad_code :4;
-            uint8_t square :1;
-            uint8_t cross :1;
-            uint8_t circle :1;
-            uint8_t triangle :1;
-
-            uint8_t l1 :1;
-            uint8_t r1 :1;
-            uint8_t l2 :1;
-            uint8_t r2 :1;
-            uint8_t l3 :1;
-            uint8_t r3 :1;
-            uint8_t ps :1;
-            uint8_t tpad_click :1;
-
-            uint8_t l2_trigger;
-            uint8_t r2_trigger;
-        } Rx_data;*/
-    private byte[] ps4_data = {0,0,0,0,0,0,0,0};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -123,6 +100,7 @@ public class MainActivity extends AppCompatActivity {
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
+                                    //receive data
                                     read_buff = msg.split(", ");
                                     if (read_buff[0].equals("VALUE")) {
                                         System.arraycopy(read_buff, 1, value_L_list, 0, 8);
@@ -133,12 +111,10 @@ public class MainActivity extends AppCompatActivity {
                                         debug.append("Get msg: " + msg + "\n");
                                         scroll.fullScroll(View.FOCUS_DOWN);
                                     }
-                                    write_buff = Byte.toString(ps4_data[0]) + "," +
-                                            Byte.toString(ps4_data[1]) + "," +
-                                            Byte.toString(ps4_data[2]) + "," +
-                                            Byte.toString(ps4_data[3]);
+
+                                    //send data
                                     try {
-                                        client.send(write_buff);
+                                        client.send(ps4.data());
                                     } catch (Exception ex) {
                                         debug.append("Exception in sending msg" + "\n");
                                         scroll.fullScroll(View.FOCUS_DOWN);
@@ -200,58 +176,117 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public boolean onGenericMotionEvent(final MotionEvent event) {
-        if ((event.getSource() & InputDevice.SOURCE_JOYSTICK) == InputDevice.SOURCE_JOYSTICK
-        && event.getAction() == MotionEvent.ACTION_MOVE) {
-            //for joystick data
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    InputDevice mInputDevice = event.getDevice();
-                    ps4_data[0] = (byte)Math.round(Byte.MAX_VALUE*getCenteredAxis(event, mInputDevice,
-                            MotionEvent.AXIS_X, -1));
+    public class PS4 {
+        /*typedef struct __attribute__ ((packed)) {
+            uint8_t hat_left_x;
+            uint8_t hat_left_y;
+            uint8_t hat_right_x;
+            uint8_t hat_right_y;
 
-                    ps4_data[1] = (byte)Math.round(Byte.MAX_VALUE*getCenteredAxis(event, mInputDevice,
-                            MotionEvent.AXIS_Y, -1));
+            uint8_t dpad_code :4;
+            uint8_t square :1;
+            uint8_t cross :1;
+            uint8_t circle :1;
+            uint8_t triangle :1;
 
-                    ps4_data[2] = (byte)Math.round(Byte.MAX_VALUE*getCenteredAxis(event, mInputDevice,
-                            MotionEvent.AXIS_Z, -1));
+            uint8_t l1 :1;
+            uint8_t r1 :1;
+            uint8_t l2 :1;
+            uint8_t r2 :1;
+            uint8_t l3 :1;
+            uint8_t r3 :1;
+            uint8_t ps :1;
+            uint8_t tpad_click :1;
 
-                    ps4_data[3] = (byte)Math.round(Byte.MAX_VALUE*getCenteredAxis(event, mInputDevice,
-                            MotionEvent.AXIS_RZ, -1));
-                    value_L_list[0] = Byte.toString(ps4_data[0]);
-                    value_L_list[1] = Byte.toString(ps4_data[1]);
-                    value_L_list[2] = Byte.toString(ps4_data[2]);
-                    value_L_list[3] = Byte.toString(ps4_data[3]);
-                    value_L.setText(cat_string(value_L_list));
-                }
-            });
-            return true;
-        } else if ((event.getSource() & InputDevice.SOURCE_DPAD) != InputDevice.SOURCE_DPAD) {
-            //for dpad
-            debug.append("get dpad" + "\n");
-            scroll.fullScroll(View.FOCUS_DOWN);
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    KeyEvent keyEvent = (KeyEvent) (InputEvent) event;
-                    if (keyEvent.getKeyCode() == KeyEvent.KEYCODE_DPAD_UP) {
-                        ps4_data[4] = 0;
-                    } else if (keyEvent.getKeyCode() == KeyEvent.KEYCODE_DPAD_RIGHT) {
-                        ps4_data[4] = 2;
-                    } else if (keyEvent.getKeyCode() == KeyEvent.KEYCODE_DPAD_DOWN) {
-                        ps4_data[4] = 4;
-                    } else if (keyEvent.getKeyCode() == KeyEvent.KEYCODE_DPAD_LEFT) {
-                        ps4_data[4] = 6;
-                    }
-                    value_L_list[4] = Byte.toString(ps4_data[4]);
-                    value_L.setText(cat_string(value_L_list));
-                }
-            });
-            return true;
+            uint8_t l2_trigger;
+            uint8_t r2_trigger;
+
+            uint8_t tpad_x;
+            uint8_t tpad_y;
+
+            uint8_t options:1;
+            uint8_t share:1;
+        } Rx_data_ps4;*/
+
+        private byte x = 0;     //AXIS_X
+        private byte y = 0;     //AXIS_Y
+        private byte x_r = 0;   //AXIS_Z
+        private byte y_r = 0;   //AXIS_RZ
+
+        private byte temp1 = 0;
+        private byte dpad_code = 8; //AXIS_HAT_X, AXIS_HAT_Y, 4 bit
+        private byte square = 0;    //1 bit
+        private byte cross = 0;     //1 bit
+        private byte circle = 0;    //1 bit
+        private byte triangle = 0;  //1 bit
+
+        private byte temp2 = 0;
+        private byte l1 = 0;
+        private byte r1 = 0;
+        private byte l2 = 0;
+        private byte r2 = 0;
+        private byte l3 = 0;
+        private byte r3 = 0;
+        private byte ps = 0;
+        private byte tpad_click = 0;
+
+        private byte l2_trigger = -127;    //AXIS_RX
+        private byte r2_trigger = -127;    //AXIS_RY
+
+        private byte tpad_x = 0;    //AXIS_GENERIC_2
+        private byte tpad_y = 0;    //AXIS_GENERIC_3
+
+        private byte temp3 = 0;
+        private byte options = 0;   //1 bit
+        private byte share = 0;     //1 bit
+        private byte commands = 0;  //6 bit
+
+        public void display() {
+            temp1 = (byte) (dpad_code << 4);
+            temp1 = (byte) (temp1 | (byte)(square << 3));
+            temp1 = (byte) (temp1 | (byte)(cross << 2));
+            temp1 = (byte) (temp1 | (byte)(circle << 1));
+            temp1 = (byte) (temp1 | triangle);
+
+            temp2 = (byte) (temp2 | (byte)(l1 << 7));
+            temp2 = (byte) (temp2 | (byte)(r1 << 6));
+            temp2 = (byte) (temp2 | (byte)(l2 << 5));
+            temp2 = (byte) (temp2 | (byte)(r2 << 4));
+            temp2 = (byte) (temp2 | (byte)(l3 << 3));
+            temp2 = (byte) (temp2 | (byte)(r3 << 2));
+            temp2 = (byte) (temp2 | (byte)(ps << 1));
+            temp2 = (byte) (temp2 | tpad_click);
+
+            temp3 = (byte) (temp3 | (byte)(options << 7));
+            temp3 = (byte) (temp3 | (byte)(share << 6));
+            temp3 = (byte) (temp3 | commands);
+
+            value_L_list[0] = Byte.toString(x);
+            value_L_list[1] = Byte.toString(y);
+            value_L_list[2] = Byte.toString(x_r);
+            value_L_list[3] = Byte.toString(y_r);
+            value_L_list[4] = Short.toString(dpad_code);
+            value_L_list[5] = Byte.toString(tpad_click);
+            value_L_list[6] = Byte.toString(l2_trigger);
+            value_L_list[7] = Byte.toString(r2_trigger);
+
+            value_R_list[0] = Byte.toString(tpad_x);
+            value_R_list[1] = Byte.toString(tpad_y);
+            value_R_list[2] = Byte.toString(temp1);
+            value_R_list[3] = Byte.toString(temp2);
+            value_R_list[4] = Byte.toString(temp3);
+            value_R_list[5] = Byte.toString(l1);
+            value_R_list[6] = Byte.toString(r2);
+            value_R_list[7] = Byte.toString(options);
+
+
+            value_L.setText(cat_string(value_L_list));
+            value_R.setText(cat_string(value_R_list));
         }
-        return super.onGenericMotionEvent(event);
+
+        public String data() {
+            return "";
+        }
     }
 
     private float getCenteredAxis(MotionEvent event,
@@ -271,5 +306,136 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return 0;
+    }
+
+    @Override
+    public boolean onGenericMotionEvent(final MotionEvent event) {
+        if ((event.getSource() & InputDevice.SOURCE_JOYSTICK) == InputDevice.SOURCE_JOYSTICK
+                && event.getAction() == MotionEvent.ACTION_MOVE) {
+            //for joystick data
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    InputDevice mInputDevice = event.getDevice();
+                    ps4.x = (byte) Math.round(Byte.MAX_VALUE * getCenteredAxis(event, mInputDevice,
+                            MotionEvent.AXIS_X, -1));
+
+                    ps4.y = (byte) Math.round(Byte.MAX_VALUE * getCenteredAxis(event, mInputDevice,
+                            MotionEvent.AXIS_Y, -1));
+
+                    ps4.x_r = (byte) Math.round(Byte.MAX_VALUE * getCenteredAxis(event, mInputDevice,
+                            MotionEvent.AXIS_Z, -1));
+
+                    ps4.y_r = (byte) Math.round(Byte.MAX_VALUE * getCenteredAxis(event, mInputDevice,
+                            MotionEvent.AXIS_RZ, -1));
+
+                    if ( event.getAxisValue(MotionEvent.AXIS_HAT_Y) == -1 ){
+                        if ( event.getAxisValue(MotionEvent.AXIS_HAT_X) == 1 ) ps4.dpad_code = 1;
+                        else if ( event.getAxisValue(MotionEvent.AXIS_HAT_X) == 0 ) ps4.dpad_code = 0;
+                        else if ( event.getAxisValue(MotionEvent.AXIS_HAT_X) == -1 ) ps4.dpad_code = 7;
+                    } else if ( event.getAxisValue(MotionEvent.AXIS_HAT_Y) == 0 ) {
+                        if ( event.getAxisValue(MotionEvent.AXIS_HAT_X) == 1 ) ps4.dpad_code = 2;
+                        else if ( event.getAxisValue(MotionEvent.AXIS_HAT_X) == 0 ) ps4.dpad_code = 8;
+                        else if ( event.getAxisValue(MotionEvent.AXIS_HAT_X) == -1 ) ps4.dpad_code = 6;
+                    } else if ( event.getAxisValue(MotionEvent.AXIS_HAT_Y) == 1 ) {
+                        if ( event.getAxisValue(MotionEvent.AXIS_HAT_X) == 1 ) ps4.dpad_code = 3;
+                        else if ( event.getAxisValue(MotionEvent.AXIS_HAT_X) == 0 ) ps4.dpad_code = 4;
+                        else if ( event.getAxisValue(MotionEvent.AXIS_HAT_X) == -1 ) ps4.dpad_code = 5;
+                    }
+
+                    ps4.l2_trigger = (byte) Math.round(Byte.MAX_VALUE * event.getAxisValue(MotionEvent.AXIS_RX));
+                    ps4.r2_trigger = (byte) Math.round(Byte.MAX_VALUE * event.getAxisValue(MotionEvent.AXIS_RY));
+
+                    ps4.tpad_x = (byte) Math.round(254 * event.getAxisValue(MotionEvent.AXIS_GENERIC_2) - 127);
+                    ps4.tpad_y = (byte) Math.round(254 * event.getAxisValue(MotionEvent.AXIS_GENERIC_3) - 127);
+
+                    ps4.display();
+                }
+            });
+            return true;
+        } else {
+            debug.append("Motion event: " + event.getAction() + " " + event.getSource() + "\n");
+            scroll.fullScroll(View.FOCUS_DOWN);
+            return super.onGenericMotionEvent(event);
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, final KeyEvent event) {
+        if (keyCode == 99) {
+            ps4.triangle = 1;
+        } else if (keyCode == 98) {
+            ps4.circle = 1;
+        } else if (keyCode == 97) {
+            ps4.cross = 1;
+        } else if (keyCode == 96) {
+            ps4.square = 1;
+        } else if (keyCode == 100) {
+            ps4.l1 = 1;
+        } else if (keyCode == 101) {
+            ps4.r1 = 1;
+        } else if (keyCode == 102) {
+            ps4.l2 = 1;
+        } else if (keyCode == 103) {
+            ps4.r2 = 1;
+        } else if (keyCode == 109) {
+            ps4.l3 = 1;
+        } else if (keyCode == 108) {
+            ps4.r3 = 1;
+        } else if (keyCode == 106) {
+            ps4.tpad_click = 1;
+        } else if (keyCode == 104) {
+            ps4.share = 1;
+        } else if (keyCode == 105) {
+            ps4.options = 1;
+        } else if (keyCode == 110) {
+            ps4.ps = 1;
+        } else {
+            debug.append("KeyDown event: " + event.getAction() + " " + event.getSource() + "\n");
+            scroll.fullScroll(View.FOCUS_DOWN);
+            return super.onKeyDown(keyCode, event);
+        }
+        ps4.display();
+        return true;
+    }
+
+
+    @Override
+    public boolean onKeyUp(int keyCode, final KeyEvent event) {
+        if (keyCode == 99) {
+            ps4.triangle = 0;
+        } else if (keyCode == 98) {
+            ps4.circle = 0;
+        } else if (keyCode == 97) {
+            ps4.cross = 0;
+        } else if (keyCode == 96) {
+            ps4.square = 0;
+        } else if (keyCode == 100) {
+            ps4.l1 = 0;
+        } else if (keyCode == 101) {
+            ps4.r1 = 0;
+        } else if (keyCode == 102) {
+            ps4.l2 = 0;
+        } else if (keyCode == 103) {
+            ps4.r2 = 0;
+        } else if (keyCode == 109) {
+            ps4.l3 = 0;
+        } else if (keyCode == 108) {
+            ps4.r3 = 0;
+        } else if (keyCode == 106) {
+            ps4.tpad_click = 0;
+        } else if (keyCode == 104) {
+            ps4.share = 0;
+        } else if (keyCode == 105) {
+            ps4.options = 0;
+        } else if (keyCode == 110) {
+            ps4.ps = 0;
+        } else {
+            debug.append("KeyDown event: " + event.getAction() + " " + event.getSource() + "\n");
+            scroll.fullScroll(View.FOCUS_DOWN);
+            return super.onKeyUp(keyCode, event);
+        }
+        ps4.display();
+        return true;
     }
 }
